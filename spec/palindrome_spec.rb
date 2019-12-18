4d1cc952746a986343c4cb68612c91277e9b79c5
Juan Piñeiro <juandpineiro@gmail.com>
2019-12-18 15:27:34 -0400

    Add title tests to pages feature spec

--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -14,6 +14,12 @@ describe 'site pages', type: :feature do
       expect(page).to have_css('h1', text: 'Sample Sinatra App')
     end
 
+    it 'has a title' do
+      visit('/')
+      @base_title = 'Learn Enough Ruby Sample App'
+      expect(page).to have_title "#{@base_title} | Home", exact: true
+    end
+
     it 'has a navbar' do
       visit('/')
       expect(page).to have_css('nav')
@@ -28,6 +34,12 @@ describe 'site pages', type: :feature do
       visit('/about')
       expect(page).to have_css('h1', text: 'About')
     end
+
+    it 'has a title' do
+      visit('/about')
+      @base_title = 'Learn Enough Ruby Sample App'
+      expect(page).to have_title "#{@base_title} | About", exact: true
+    end
   end
 
   context 'palindrome' do
@@ -37,6 +49,12 @@ describe 'site pages', type: :feature do
     it 'has a header' do
       visit('/palindrome')
       expect(page).to have_css('h1', text: 'Palindrome Detector')
+    end
+
+    it 'has a title' do
+      visit('/palindrome')
+      @base_title = 'Learn Enough Ruby Sample App'
+      expect(page).to have_title "#{@base_title} | Palindrome Detector", exact: true
     end
   end
 end
